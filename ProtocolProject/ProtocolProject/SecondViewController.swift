0d9339c095f8a80ac28a0790c445130e64e4a92f
Benny <benny@bdapps.net>
2017-01-05 21:09:44 +0200

    5.1.2017

--- ProtocolProject/ProtocolProject/SecondViewController.swift
+++ ProtocolProject/ProtocolProject/SecondViewController.swift
@@ -8,28 +8,25 @@
 
 import UIKit
 
+protocol SecondViewControllerDelegate {
+    func secondVC(_ controller : SecondViewController, didFinishWith text : String?)
+}
+
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var delegate : SecondViewControllerDelegate?
+    
+    @IBAction func doneAction(_ sender: UITextField) {
+        //notify delegate
+        delegate?.secondVC(self, didFinishWith: sender.text)
+        //opposite of present modally
+        self.dismiss(animated: true, completion: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+
+
+
